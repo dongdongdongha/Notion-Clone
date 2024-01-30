9158cce2886497aba58f45ce90b4ff386a4a0a52
류동하 <06dongha@gmail.com>
2024-01-30 23:49:02 +0900

    0130

---
+++ notion/app/_components/page.tsx
@@ -0,0 +1,5 @@
+const ComponentThing = () => {
+  return <div>T is s t b a C</div>;
+};
+
+export default ComponentThing;
---
+++ notion/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://darling-termite-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
